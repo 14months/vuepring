563aa07d158990a3816737aafab0bd4808d6edcc
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2023-05-11 22:25:42 +0900

    Initial commit

---
+++ src/main/java/com/vuepring/VuepringApplication.java
@@ -0,0 +1,13 @@
+package com.vuepring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuepringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuepringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vuepring/VuepringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuepring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuepringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
